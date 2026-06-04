e7265bc4c4d0ee9a3aafa37233f3eef64062af1a
wangzhipeng <wangzhipeng@wondersgroup.com>
2026-06-04 16:56:21 +0800

    Fesod: 简单写入和Sheet页相关不同操作

---
+++ src/main/java/org/example/demospring/excel/fesod/DemoData.java
@@ -0,0 +1,30 @@
+package org.example.demospring.excel.fesod;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.fesod.sheet.annotation.ExcelIgnore;
+import org.apache.fesod.sheet.annotation.ExcelProperty;
+
+import java.util.Date;
+
+/**
+ * @ClassName DemoData
+ * @Description TODO
+ * @Author wangzhipeng
+ * @Date 2026/6/4 16:09
+ */
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class DemoData {
+    @ExcelProperty("字符串标题")
+    private String string;
+    @ExcelProperty("日期标题")
+    private Date date;
+    @ExcelProperty("数字标题")
+    private Double doubleData;
+    @ExcelIgnore
+    private String ignore; // 忽略的字段
+}
--- src/test/java/org/example/demospring/DemoSpringApplicationTests.java
+++ src/test/java/org/example/demospring/DemoSpringApplicationTests.java
@@ -1,13 +1,92 @@
 package org.example.demospring;
 
+import org.apache.fesod.common.util.ListUtils;
+import org.apache.fesod.sheet.ExcelWriter;
+import org.apache.fesod.sheet.FesodSheet;
+import org.apache.fesod.sheet.write.metadata.WriteSheet;
+import org.apache.fesod.sheet.write.metadata.WriteTable;
+import org.example.demospring.excel.fesod.DemoData;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+import java.util.List;
 
 @SpringBootTest
 class DemoSpringApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    private List<DemoData> data() {
+        List<DemoData> list = ListUtils.newArrayList();
+        for (int i = 0; i < 10; i++) {
+            DemoData data = new DemoData();
+            data.setString("String" + i);
+            data.setDate(new Date());
+            data.setDoubleData(0.56);
+            list.add(data);
+        }
+        return list;
+    }
+
+
+    @Test
+    public void simpleWrite() {
+        String fileName = "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+
+        FesodSheet.write(fileName, DemoData.class)
+                .sheet("Sheet1")
+                .doWrite(data());
+    }
+
+    @Test
+    public void simpleWriteExcelWriter() {
+        String fileName = "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+
+        try (ExcelWriter excelWriter = FesodSheet.write(fileName, DemoData.class).build()) {
+            WriteSheet writeSheet = FesodSheet.writerSheet("Sheet1").build();
+            excelWriter.write(data(), writeSheet);
+        }
+    }
+
+    @Test
+    public void writeSingleSheet() {
+        String fileName = "repeatedWrite" + System.currentTimeMillis() + ".xlsx";
+
+        try (ExcelWriter excelWriter = FesodSheet.write(fileName, DemoData.class).build()) {
+            WriteSheet writeSheet = FesodSheet.writerSheet("Sheet1").build();
+            for (int i = 0; i < 5; i++) {
+                excelWriter.write(data(), writeSheet);
+            }
+        }
+    }
+
+    @Test
+    public void writeMultiSheet() {
+        String fileName = "repeatedWrite" + System.currentTimeMillis() + ".xlsx";
+
+        try (ExcelWriter excelWriter = FesodSheet.write(fileName, DemoData.class).build()) {
+            for (int i = 0; i < 5; i++) {
+                WriteSheet writeSheet = FesodSheet.writerSheet("Sheet" + i).build();
+                excelWriter.write(data(), writeSheet);
+            }
+        }
+    }
+
+    // TODO 这个有bug，他会在第一行多生成一个标题行
+    @Test
+    public void tableWrite() {
+        String fileName = "repeatedWrite" + System.currentTimeMillis() + ".xlsx";
+
+        try (ExcelWriter excelWriter = FesodSheet.write(fileName, DemoData.class).build()) {
+            WriteSheet writeSheet = FesodSheet.writerSheet("Table示例").build();
+            WriteTable table0 = FesodSheet.writerTable(0).build();
+            WriteTable table1 = FesodSheet.writerTable(1).build();
+            excelWriter.write(data(), writeSheet, table0);
+            excelWriter.write(data(), writeSheet, table1);
+        }
     }
 
 }
